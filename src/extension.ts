33684d61e644fe986c2ad05b581aafe0a9388d37
Kuuhaku <ryusama6996@gmail.com>
2024-12-12 19:12:15 +0530

    fghjkl

--- src/extension.ts
+++ src/extension.ts
@@ -24,6 +24,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+
 	const disposable = vscode.commands.registerCommand('idfk-v1.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
@@ -33,19 +34,10 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 
 	});
-	const log = vscode.commands.registerCommand('idfk-v1.log', async () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// vscode.window.showInputBox().then(async e => {
-		// const currentDir = 
-		await execShell(`git log`);
-		// vscode.window.showInformationMessage(`Done`);
-		// })
 
-	});
 
 	context.subscriptions.push(disposable);
-	context.subscriptions.push(log);
+	// context.subscriptions.push(log);
 
 }
 
