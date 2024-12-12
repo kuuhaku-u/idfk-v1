e0d9de25b6b7de8257ea5aadb6cc9000f0fe3a7d
Kuuhaku <ryusama6996@gmail.com>
2024-12-12 19:45:34 +0530

    fff

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as cp from "child_process";
 
@@ -14,45 +12,42 @@ const execShell = (cmd: string) =>
 	});
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "idfk-v1" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-
 	const disposable = vscode.commands.registerCommand('idfk-v1.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInputBox().then(async e => {
-			if (e) {
-				await execShell(`git add . && git commit -m "${e}"`);
-				vscode.window
-					.showInformationMessage('Commited', ...['Push', 'Cancel'])
-					.then(selection => {
-						if (selection === "Push") {
-							console.log(selection);
+		execShell('git status --porcelain')
+			.then(status => {
+				if (!status) {
+					vscode.window.showInformationMessage('No changes to commit.');
+				} else {
+					vscode.window.showInputBox().then(async message => {
+						if (message) {
+							try {
+								await execShell(`git add . && git commit -m "${message}"`);
+								vscode.window.showInformationMessage('Committed', ...['Push', 'Cancel'])
+									.then(selection => {
+										if (selection === "Push") {
+											console.log("Push selected");
+										} else {
+											console.log("Canceled");
+										}
+									});
+							} catch (err: any) {
+								vscode.window.showErrorMessage(`Error committing: ${err.message}`);
+							}
 						} else {
-							console.log("CANCELED");
+							vscode.window.showInformationMessage('Commit aborted');
 						}
 					});
-			} else {
-				vscode.window.showInformationMessage(`Aborted`);
-
-			}
-		})
-
+				}
+			})
+			.catch(err => {
+				vscode.window.showErrorMessage(`Error checking git status: ${err.message}`);
+			});
 	});
 
-
 	context.subscriptions.push(disposable);
-	// context.subscriptions.push(log);
-
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() { }
